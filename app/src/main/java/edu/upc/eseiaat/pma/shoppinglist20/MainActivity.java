96a515881d0435e518f6320aa8d4e371ff0e0404
EddieMG <eduardmgobierno@gmail.com>
2018-01-02 17:36:11 +0100

    Commit 2

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist20/MainActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist20/MainActivity.java
@@ -11,6 +11,7 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewDebug;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,12 +21,22 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static android.provider.Telephony.Mms.Part.FILENAME;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MAX_BYTES =8000 ;
     ArrayList<HashMap<String,String>>data;
     private String[] titleArray,subItemArray;
     private ListView list;
@@ -36,7 +47,52 @@ public class MainActivity extends AppCompatActivity {
     private AlertDialog.Builder alert;
     private Integer PreuTotal;
     private TextView Total;
-
+//    private void writeItemList(){
+//
+//       try {
+//           FileOutputStream fos =openFileOutput(FILENAME, Context.MODE_PRIVATE);
+//            DataOutputStream dout = new DataOutputStream(fos);
+//            dout.write(data); // Save line count
+//            dout.flush(); // Flush stream ...
+//            dout.close(); // ... and close.
+//           fos.close();
+//       } catch (FileNotFoundException e) {
+//           Log.e("Eddie","writeItemList: FileNotFoundException");
+////
+//      } catch (IOException e) {
+//           Log.e("Eddie","writeItemList: IOException ");
+////
+//       }
+////
+//   }
+////
+//
+//    private  void readItemList(){
+//        itemlist= new ArrayList<>();
+//        try {
+//            FileInputStream fis= openFileInput(FILENAME);
+//            byte [] buffer= new byte[MAX_BYTES];
+//            int nread=fis.read(buffer);
+//            if(nread>0 ) {
+//                String content = new String(buffer, 0, nread);
+//                String[] lines = content.split("\n");
+//                for (String line : lines) {
+//                    String[] parts = line.split(";");
+//                    itemlist.add(new ShoppingItem(parts[0], parts[1].equals("true")));
+//
+//                }
+//            }
+//            fis.close();
+//        } catch (FileNotFoundException e) {
+//            Log.i("Eddie","readItemList: FileNotFoundException ");
+//
+//        } catch (IOException e) {
+//            Log.e("Eddie","readItemList: IOException ");
+//
+//        }
+//
+//
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,27 +101,21 @@ public class MainActivity extends AppCompatActivity {
         PreuTotal=0;
         list = (ListView)findViewById(R.id.list);
         btn_add = (Button)findViewById(R.id.btn_add);
-        data = new ArrayList<HashMap<String, String>>();
-//        titleArray=new String[]{"Test1","Test2"};
-//        subItemArray=new String[]{"SubTest1","SubTest2"};
+        data = new ArrayList<HashMap<String,String>>();
+
         Total=(TextView) findViewById(R.id.textView);
-//        for(int i=0;i<titleArray.length;i++){
-//            datum = new HashMap<String, String>();
-//            datum.put("title", titleArray[i]);
-//            datum.put("preu", subItemArray[i]);
-//            data.add(datum);
-//        }
+
 
         adapter= new SimpleAdapter(this,data,android.R.layout.simple_list_item_2,new String[]{"title","preu"},new int[]{android.R.id.text1,android.R.id.text2});
         list.setAdapter(adapter);
 
-    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            Log.i("Eddie", String.format("Has seleccionat %s",data.get(i)));
-
-        }
-    });
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View view, int pos, long id) {
+                maybeRemoveItem(pos);
+                return true;
+            }
+        });
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,15 +177,19 @@ public class MainActivity extends AppCompatActivity {
 
                 alert.show();
             }
-        });
-}
-
-    private void addItem() {
+        });}
 
 
+    private void maybeRemoveItem(final int pos) {
 
 
+
+
+
+                data.remove(pos);
+                adapter.notifyDataSetChanged();
+            }
 
 
     }
-}
+
